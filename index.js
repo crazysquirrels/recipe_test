7584c89cdd7d7c03cd24cbb4f1b75e9c3a5f47e8
An, Albert <alban@visa.com>
2017-08-16 15:51:48 -0700

    working now

--- index.js
+++ index.js
@@ -11,24 +11,29 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var connect = "postgres://mijswudcvxfgif:4bfebe8d5e7f9d360439460e8d4bf871501ae8671e8766e098ebdfdc0604a301@ec2-23-23-227-188.compute-1.amazonaws.com:5432/da6en3ip038jf8";
-
+//var connect = "postgres://mijswudcvxfgif:4bfebe8d5e7f9d360439460e8d4bf871501ae8671e8766e098ebdfdc0604a301@ec2-23-23-227-188.compute-1.amazonaws.com:5432/da6en3ip038jf8";
+var connect ="postgres://alban:Visa52197@localhost:5432/recipebookdb";
 
 app.get('/', function(request, response) {
   //response.render('pages/index');
+  var client = new pg.Client(connect);
+  client.connect(function(err) {
+      if (err) throw err;
 
       client.query('SELECT * FROM recipes', function(err, result) {
         if (err) throw err;
         // just print the result to the console
-        console.log(result); // outputs: { name: 'brianc' }
+        console.log(result.rows[0]); // outputs: { name: 'brianc' }
         // disconnect the client
         client.end(function(err) {
             if (err) throw err;
         });
-        res.render('pages/index', {
+        response.render('pages/index', {
             recipes: result.rows
         });
     }); //end query
+
+});
 
 });
 
